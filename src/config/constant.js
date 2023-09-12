504e066f611024a5bccb8b1a53aa3719af62bfa8
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-09-12 15:43:36 -0300

    Update constant.js

--- src/config/constant.js
+++ src/config/constant.js
@@ -2,7 +2,7 @@ let BACKEND_SERVER = null;
 if (process.env.REACT_APP_BACKEND_SERVER) {
   BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
 } else {
-  BACKEND_SERVER = "https://fierce-pink-tortoise.cyclic.cloud/";
+  BACKEND_SERVER = "https://odd-cyan-toad-wig.cyclic.cloud/";
 }
 
 export const API_SERVER = BACKEND_SERVER;
