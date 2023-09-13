6f8bc85bcc57f3f05c3955398398ac0ec4961ca3
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-09-13 15:21:30 -0300

    Update constant.js

--- src/config/constant.js
+++ src/config/constant.js
@@ -2,7 +2,7 @@ let BACKEND_SERVER = null;
 if (process.env.REACT_APP_BACKEND_SERVER) {
   BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
 } else {
-  BACKEND_SERVER = "https://dd85-2804-431-f731-f5d-c5b7-4368-d375-7984.ngrok.io/";
+  BACKEND_SERVER = "https://38c3-2804-431-f731-f5d-c5b7-4368-d375-7984.ngrok.io/";
 }
 
 export const API_SERVER = BACKEND_SERVER;
